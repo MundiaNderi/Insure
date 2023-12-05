9ac61c37441cf84b2bc649ca9748a0768eb9ee12
Serah Nderi <sarahngima77@gmail.com>
2023-12-05 10:02:44 +0300

    Insure

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens : {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        purple: '#2D2641;',
+        darkPurple: '#2C2830',
+        lightGray: '#837D88',
+        offWhite: 'FAFAFA',
+        lightPurple: '#C396C6'
+      },
+      textColor: ['responsive', 'hover', 'focus', 'group-hover'],
+    },
+  },
+  plugins: [],
+}
